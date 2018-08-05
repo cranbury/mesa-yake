a2c40075c77b495f77d983ea4f1e9a3e93f3d238
Rakzodia <jeec.estaba@gmail.com>
2018-08-05 03:59:11 +0000

    [feature] See all and create products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,19 @@
 class Admin::ProductsController < ApplicationController
+    def index
+        @products = Product.all
+    end
+    
+    def new
+        @product = Product.new
+    end
+    
+    def create
+        Product.create(product_params)
+        redirect_to restaurant_admin_products_path
+    end
+    
+    private
+    def product_params
+        params.require(:product).permit(:name, :precio)
+    end
 end
