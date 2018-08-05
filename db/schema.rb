aa7345aed782c0b32ced7faf25c970347bde279e
Granger <GrangerAbuhoff@gmail.com>
2018-08-05 10:34:22 -0500

    get tables available

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_05_012507) do
+ActiveRecord::Schema.define(version: 2018_08_05_150258) do
 
   create_table "meals", force: :cascade do |t|
     t.integer "mesa_id"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2018_08_05_012507) do
     t.string "name"
     t.integer "restaurant_id"
     t.integer "seats"
-    t.boolean "abierto"
+    t.boolean "available"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
