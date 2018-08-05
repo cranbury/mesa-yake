92c5db0f23d97d72be95de082a6892f137650d64
Rakzodia <jeec.estaba@gmail.com>
2018-08-05 00:59:17 +0000

    [add] Admin views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    before_action :check_admin
+    
+    def check_admin
+        @admin = params["controller"].split("/")[0] == "admin"
+    end
 end
