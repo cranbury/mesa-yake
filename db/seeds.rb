acee6e6b57305cb012c99d140235740ab4f49cb4
invMou <shopir9@hotmail.com>
2018-08-05 11:52:20 -0500

    show the tables for restaurant, for the demostration

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,4 +21,7 @@ Product.find_or_create_by(restaurant_id: 2, name:"Mango Sago Pomelo Dessert æ¨æ
 Mesa.find_or_create_by(restaurant_id: 1,name: "Table 1 (Lobby)", seats: 1, abierto:true)
 Mesa.find_or_create_by(restaurant_id: 1,name: "Table 2 (Bar)",  seats: 1, abierto:false)
 
-Mesa.create!(restaurant_id: 1, seats: 4, abierto: true)
+Mesa.create!(restaurant_id: 2, seats: 4, abierto: true)
+Mesa.create!(restaurant_id: 2, seats: 4, abierto: true)
+Mesa.create!(restaurant_id: 3, seats: 4, abierto: true)
+Mesa.create!(restaurant_id: 3, seats: 4, abierto: true)
