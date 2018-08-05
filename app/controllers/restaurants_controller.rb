a92e9949d12f1dbc3c3cea01fc5e0eb354e4318f
Esteban Samuel Reyes <estebansamuel.reyes@gmail.com>
2018-08-05 13:57:29 -0500

    kitchen view

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -25,12 +25,7 @@ class RestaurantsController < ApplicationController
   def meal
     mesa_id = params[:mesa_id]
     mesa = Mesa.find_by(id: mesa_id)
-<<<<<<< HEAD
     mesa.available= true
-=======
-    mesa.available = true
->>>>>>> a1183e9f6c3004443466c049fee01d3293cbeffa
-    mesa.save
 
     Meal.create(mesa_id:mesa_id)
     redirect_to "#{restaurant_mesa_index_path}/#{mesa_id}"
